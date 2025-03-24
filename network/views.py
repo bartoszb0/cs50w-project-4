b86e57425acc9b6e562f699f7159eca28db25726
bartoszb0 <bartoszb2020@gmail.com>
2025-03-24 21:01:18 +0100

    Finished

--- network/views.py
+++ network/views.py
@@ -13,14 +13,18 @@ from .models import User, Post
 
 def index(request):
     all_posts = Post.objects.all().order_by("-timestamp")
+
     if request.method == "POST":
         content = request.POST["content"]
         new_post = Post(content=content, creator=request.user)
         new_post.save()
         return HttpResponseRedirect(reverse('index'))
     else:
+        page = request.GET.get('page')
+        paginator = Paginator(all_posts, 10)
+        page_obj = paginator.get_page(page)
         return render(request, "network/index.html", {
-            "all_posts": all_posts
+            "all_posts": page_obj
         })
 
 
@@ -79,10 +83,13 @@ def register(request):
 def user_account(request, username):
     user = User.objects.get(username=username)
     all_posts = Post.objects.filter(creator = user.id).order_by("-timestamp")
+    page = request.GET.get('page')
+    paginator = Paginator(all_posts, 10)
+    page_obj = paginator.get_page(page)
     return render(request, "network/user_account.html", {
         "show_user": user,
-        "all_posts": all_posts,
-        "post_count": len(all_posts),
+        "all_posts": page_obj,
+        "post_count": all_posts.count(),
         "is_followed": user.followers.filter(username = request.user.username).exists()
     })
 
@@ -91,8 +98,11 @@ def user_account(request, username):
 def followed_posts(request):
     user = User.objects.get(username=request.user.username)
     all_posts = Post.objects.filter(creator__in=user.following.all()).order_by("-timestamp")
+    page = request.GET.get('page')
+    paginator = Paginator(all_posts, 10)
+    page_obj = paginator.get_page(page)
     return render(request, "network/followed_posts.html", {
-        "all_posts": all_posts
+        "all_posts": page_obj
     })
 
 
@@ -119,11 +129,9 @@ def follow(request):
         if follow_request.following.filter(username=follow_who.username).exists():
             follow_request.following.remove(follow_who)
             follow_who.followers.remove(follow_request)
-            print(f"{follow_request} just unfollowed {follow_who}") #TODO
         else:
             follow_request.following.add(follow_who)
             follow_who.followers.add(follow_request)
-            print(f"{follow_request} just followed {follow_who}") #TODO
 
         follow_request.save()
         follow_who.save()
@@ -142,10 +150,8 @@ def like_post(request):
         
         if liked_post.likes.filter(username = liked_by.username).exists():
             liked_post.likes.remove(liked_by)
-            print(f"{liked_by} unliked {liked_post}")
         else:
             liked_post.likes.add(liked_by)
-            print(f"{liked_by} liked {liked_post}")
 
         liked_post.save()
 
